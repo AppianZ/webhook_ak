9a0bb6d6410e2025474e1b3c5eb8e0df34147a1d
Appian <zhang4baichi@sina.com>
2017-09-08 14:07:16 +0800

    test16

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+WEB_PATH='/root/tools/'$1
+WEB_USER='root'
+WEB_USERGROUP='root'
+
 echo "Start deployment"
+cd $WEB_PATH
+echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
 git pull
+git checkout master
+echo "changing permissions..."
+chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
 echo "Finished."
