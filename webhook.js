013d87f8b3001fc85357e41286f3cde97a111c67
Appian <zhang4baichi@sina.com>
2017-09-11 13:49:37 +0800

    test 3

--- webhook.js
+++ webhook.js
@@ -43,7 +43,7 @@ handler.on('push', function (event) {
   switch (path) {
     case '/webhook':
       webhook_cmd('/home/appian/web/Close2Webhook', function () {
-        process.exec('pm2 restart 7', function (error, stdout, stderr) {
+        process.exec('pm2 restart appian.webhook', function (error, stdout, stderr) {
           if (error) console.log('this error in' + event.payload.repository.name, error);
           else console.log('/webhook 的 pm2 重启成功');
         });
